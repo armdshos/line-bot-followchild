3fa59fe5ed35a5eb607f26df77d01828591be224
panu pakdeesan <arm.panu@gmail.com>
2020-11-23 14:22:17 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,7 +3,7 @@
 
 $access_token = 'tg3ul2zoeExQRu3WyyyTRQd0mCXZ7L9cCIlbCXV8Ov16XI2ufyt2FjP/HNhF3NhAiWE+csTRZmxe9bnggvT5DCGw/eezuYA8SKu1pgu1NfesWpvkQxfYogpaxU9PKgsPNpKavbXuQCrMqPl2xSBEmQdB04t89/1O/w1cDnyilFU=';
 
-$userId = '8e12c625670c0b6bd0eed3942494169a';
+$userId = 'U2947c469fd8429f2ab326d98a5308d97';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
